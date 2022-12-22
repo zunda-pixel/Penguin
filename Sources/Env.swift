cdefae7b63334e204c9f11e9e40e24c3a5848c64
zunda <zunda.dev@gmail.com>
2022-12-22 23:46:58 +0900

    add appGroups, schemeURL

--- Sources/Env.swift
+++ Sources/Env.swift
@@ -7,4 +7,7 @@ import Foundation
 struct Env {
   static let clientKey: String = <#CLIENT_KEY#>
   static let clientSecretKey: String = <#CLIENT_SECRET_KEY#>
+  
+  static let appGroups: String = "group.zunda.penguin"
+  static let schemeURL: URL = .init(string: "penguin://")!
 }
