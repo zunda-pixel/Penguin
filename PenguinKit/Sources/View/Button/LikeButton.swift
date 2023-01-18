1923d6216fc2edca06541a8fb919d1f3ec518502
zunda <zunda.dev@gmail.com>
2023-01-18 17:23:26 +0900

    add UnLikeButton

--- PenguinKit/Sources/View/Button/LikeButton.swift
+++ PenguinKit/Sources/View/Button/LikeButton.swift
@@ -31,3 +31,30 @@ struct LikeButton: View {
     }
   }
 }
+
+struct UnLikeButton: View {
+  @Binding var errorHandle: ErrorHandle?
+
+  let userID: String
+  let tweetID: String
+
+  func action() async {
+    do {
+      try await Sweet(userID: userID).unLike(userID: userID, tweetID: tweetID)
+    } catch {
+      let errorHandle = ErrorHandle(error: error)
+      errorHandle.log()
+      self.errorHandle = errorHandle
+    }
+  }
+
+  var body: some View {
+    Button {
+      Task {
+        await action()
+      }
+    } label: {
+      Label("UnLike", systemImage: "heart.slash")
+    }
+  }
+}
