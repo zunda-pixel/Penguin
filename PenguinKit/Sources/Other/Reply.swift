0611208edeb495efdbb67072883cb50e51173322
zunda <zunda.dev@gmail.com>
2023-01-25 17:05:44 +0900

    sort when ini

--- PenguinKit/Sources/Other/Reply.swift
+++ PenguinKit/Sources/Other/Reply.swift
@@ -10,4 +10,13 @@ struct Reply: Identifiable {
   let replyID: String
   let ownerID: String
   let replyUsers: [Sweet.UserModel]
+  
+  init(replyID: String, ownerID: String, replyUsers: [Sweet.UserModel]) {
+    self.replyID = replyID
+    self.ownerID = ownerID
+    // ownerが先頭に来るようにする
+    self.replyUsers = replyUsers.sorted { user1, user2 in
+      user1.id == ownerID && user2.id != ownerID
+    }
+  }
 }
