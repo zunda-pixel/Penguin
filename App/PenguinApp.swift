53102d1dc5b3e53432f21d767657019a7bac97d5
zunda <zunda.dev@gmail.com>
2023-02-09 15:55:37 +0900

    add subscriptionExpireDate

--- App/PenguinApp.swift
+++ App/PenguinApp.swift
@@ -13,15 +13,26 @@ struct PenguinApp: App {
   @State var settings: PenguinKit.Settings = Secure.settings
   @State var currentUser: Sweet.UserModel? = Secure.currentUser
   @State var loginUsers: [Sweet.UserModel] = Secure.loginUsers
+  @State var subscriptionExpireDate: Date? = Secure.subscriptionExpireDate
   
   var body: some Scene {
     WindowGroup {
-      ContentView(settings: $settings, currentUser: $currentUser, loginUsers: $loginUsers)
+      ContentView(
+        settings: $settings,
+        currentUser: $currentUser,
+        loginUsers: $loginUsers,
+        subscriptionExpireDate: $subscriptionExpireDate
+      )
         .handlesExternalEvents(preferring: ["penguin"], allowing: []) // prevent to generate new windows from url scheme
     }
 #if os(macOS)
     SwiftUI.Settings {
-      SettingsView(settings: $settings, currentUser: $currentUser, loginUsers: $loginUsers)
+      SettingsView(
+        settings: $settings,
+        currentUser: $currentUser,
+        loginUsers: $loginUsers,
+        subscriptionExpireDate: $subscriptionExpireDate
+      )
     }
 #endif
   }
