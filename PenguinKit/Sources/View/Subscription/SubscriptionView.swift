2e926bca839d4d68954a9c5e2b78fd991dc38be1
zunda <zunda.dev@gmail.com>
2023-02-09 16:59:31 +0900

    add SubscriptionView

---
+++ PenguinKit/Sources/View/Subscription/SubscriptionView.swift
@@ -0,0 +1,73 @@
+//
+//  SubscriptionView.swift
+//
+
+import SwiftUI
+
+public struct SubscriptionView: View {
+  @StateObject var viewModel = SubscriptionViewModel()
+  @Binding var subscriptionExpireDate: Date?
+  
+  #if DEBUG
+  @State var isPresentedManageSubscription: Bool = false
+  #endif
+  
+  public init(expireDate subscriptionExpireDate: Binding<Date?>) {
+    self._subscriptionExpireDate = subscriptionExpireDate
+  }
+  
+  public var body: some View {
+    VStack {
+#if os(macOS)
+      let iconName = NSApplication.shared.iconName
+#else
+      let iconName = UIApplication.shared.iconName
+#endif
+      Image(iconName, bundle: .module)
+        .resizable()
+        .scaledToFit()
+        .cornerRadius(15)
+        .padding(40)
+      
+      ForEach(viewModel.products) { product in
+        Button {
+          Task {
+            let transaction = await viewModel.purchase(product: product)
+            subscriptionExpireDate = transaction?.expirationDate
+            Secure.subscriptionExpireDate = transaction?.expirationDate
+          }
+        } label: {
+          ProductCell(product: product)
+        }
+        .buttonStyle(.borderedProminent)
+        .buttonBorderShape(.roundedRectangle)
+        .padding(.horizontal, 40)
+      }
+      
+      #if DEBUG
+      Button {
+        isPresentedManageSubscription.toggle()
+      } label: {
+        Label("Manage Subscription", systemImage: "gear")
+      }
+      .manageSubscriptionsSheet(isPresented: $isPresentedManageSubscription)
+      .buttonStyle(.borderedProminent)
+      .buttonBorderShape(.roundedRectangle)
+      .padding(.horizontal, 40)
+      #endif
+    }
+    .frame(maxWidth: .infinity, maxHeight: .infinity)
+    .background(Color.indigo.opacity(0.3))
+    .alert(errorHandle: $viewModel.errorHandle)
+    .task {
+      await viewModel.fetchProducts()
+    }
+  }
+}
+
+
+struct ManageSubscriptionView_Preview: PreviewProvider {
+  static var previews: some View {
+    SubscriptionView(expireDate: .constant(.now))
+  }
+}
