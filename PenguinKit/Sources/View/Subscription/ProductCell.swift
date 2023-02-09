9d3e54437f821f70ed76cf7f339cc0a00394b347
zunda <zunda.dev@gmail.com>
2023-02-09 16:56:19 +0900

    add ProductCell

---
+++ PenguinKit/Sources/View/Subscription/ProductCell.swift
@@ -0,0 +1,27 @@
+//
+//  ProductCell.swift
+//
+
+import SwiftUI
+import StoreKit
+
+struct ProductCell: View {
+  let product: Product
+  
+  var body: some View {
+    HStack {
+      VStack(alignment: .leading) {
+        Text(product.displayName)
+        
+        if let period = product.subscription?.subscriptionPeriod {
+          Text("\(period.value) \(period.unit.localizedDescription)")
+            .font(.callout)
+        }
+      }
+      
+      Spacer()
+      
+      Text("\(product.displayPrice)")
+    }
+  }
+}
