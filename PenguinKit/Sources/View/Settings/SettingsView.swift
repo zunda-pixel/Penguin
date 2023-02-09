383981de274f1b496bb07a9edb013becc81af717
zunda <zunda.dev@gmail.com>
2023-02-10 01:08:13 +0900

    add test CheckSubscription

--- PenguinKit/Sources/View/Settings/SettingsView.swift
+++ PenguinKit/Sources/View/Settings/SettingsView.swift
@@ -110,6 +110,12 @@ public struct SettingsView: View {
           }
 
           Section("ABOUT") {
+            NavigationLink {
+              CheckSubscription()
+            } label: {
+              Text("Check Subscription")
+            }
+            
             #if !os(macOS)
               Button {
                 isPresentedManageSubscription.toggle()
@@ -194,4 +200,35 @@ struct SettingsView_Preview: PreviewProvider {
   static var previews: some View {
     Preview()
   }
+}
+
+struct CheckSubscription: View {
+  @State var expireDate: Date?
+  @State var text: String = ""
+  
+  var body: some View {
+    VStack {
+      Button("Check") {
+        Task {
+          text = "Loading"
+          
+          let result = await SubscribeManager.purchasedProducts()
+          
+          guard let result else {
+            expireDate = nil
+            return
+          }
+          
+          self.expireDate =  try? result.payloadValue.expirationDate
+          
+          text = "Finish"
+        }
+      }
+      
+      if let expireDate {
+        Text(expireDate, format: .dateTime)
+      }
+    }
+  }
+    
 }
