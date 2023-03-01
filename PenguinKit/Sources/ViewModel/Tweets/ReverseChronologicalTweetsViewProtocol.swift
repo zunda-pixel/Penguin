db1725d96f64fa29ec948bf1a96ed9a6b689d11a
zunda <zunda.dev@gmail.com>
2023-03-02 01:38:23 +0900

    引用先のツイートが非公開アカウントのツイートの可能性があるためif letアンラップ

--- PenguinKit/Sources/ViewModel/Tweets/ReverseChronologicalTweetsViewProtocol.swift
+++ PenguinKit/Sources/ViewModel/Tweets/ReverseChronologicalTweetsViewProtocol.swift
@@ -197,8 +197,8 @@ extension ReverseChronologicalTweetsViewProtocol {
 
     let quotedQuotedTweet: TweetContentModel?
 
-    if let quoted = tweet.referencedTweets.first(where: { $0.type == .quoted }) {
-      let tweet = getTweet(quoted.id)!
+    if let quoted = tweet.referencedTweets.first(where: { $0.type == .quoted }),
+       let tweet = getTweet(quoted.id) /* TODO 引用先のツイートが非公開アカウントのツイートの可能性があるためif letアンラップ */{
       let user = getUser(tweet.authorID!)!
       quotedQuotedTweet = TweetContentModel(tweet: tweet, author: user)
     } else {
