3f5f5afae69de4fb8e1749fbe49fb790d508d62e
zunda <zunda.dev@gmail.com>
2023-03-11 13:43:50 +0900

    不要な変数を削除

--- PenguinKit/Sources/ViewModel/Tweets/ReverseChronologicalViewModel.swift
+++ PenguinKit/Sources/ViewModel/Tweets/ReverseChronologicalViewModel.swift
@@ -14,11 +14,6 @@ final class ReverseChronologicalViewModel: NSObject, ReverseChronologicalTweetsV
   let viewContext: NSManagedObjectContext
   let fetchTimelineController: NSFetchedResultsController<Timeline>
   let fetchShowTweetController: NSFetchedResultsController<Tweet>
-  let fetchTweetController: NSFetchedResultsController<Tweet>
-  let fetchUserController: NSFetchedResultsController<User>
-  let fetchMediaController: NSFetchedResultsController<Media>
-  let fetchPollController: NSFetchedResultsController<Poll>
-  let fetchPlaceController: NSFetchedResultsController<Place>
 
   @Published var searchSettings: TimelineSearchSettings
   @Published var loadingTweets: Bool
@@ -60,88 +55,13 @@ final class ReverseChronologicalViewModel: NSObject, ReverseChronologicalTweetsV
       )
     }()
 
-    self.fetchTweetController = {
-      let fetchRequest = NSFetchRequest<Tweet>()
-      fetchRequest.entity = Tweet.entity()
-      fetchRequest.sortDescriptors = []
-
-      return NSFetchedResultsController<Tweet>(
-        fetchRequest: fetchRequest,
-        managedObjectContext: viewContext,
-        sectionNameKeyPath: nil,
-        cacheName: nil
-      )
-    }()
-
-    self.fetchUserController = {
-      let fetchRequest = NSFetchRequest<User>()
-      fetchRequest.entity = User.entity()
-      fetchRequest.sortDescriptors = []
-
-      return NSFetchedResultsController<User>(
-        fetchRequest: fetchRequest,
-        managedObjectContext: viewContext,
-        sectionNameKeyPath: nil,
-        cacheName: nil
-      )
-    }()
-
-    self.fetchPollController = {
-      let fetchRequest = NSFetchRequest<Poll>()
-      fetchRequest.entity = Poll.entity()
-      fetchRequest.sortDescriptors = []
-
-      return NSFetchedResultsController<Poll>(
-        fetchRequest: fetchRequest,
-        managedObjectContext: viewContext,
-        sectionNameKeyPath: nil,
-        cacheName: nil
-      )
-    }()
-
-    self.fetchMediaController = {
-      let fetchRequest = NSFetchRequest<Media>()
-      fetchRequest.entity = Media.entity()
-      fetchRequest.sortDescriptors = []
-
-      return NSFetchedResultsController<Media>(
-        fetchRequest: fetchRequest,
-        managedObjectContext: viewContext,
-        sectionNameKeyPath: nil,
-        cacheName: nil
-      )
-    }()
-
-    self.fetchPlaceController = {
-      let fetchRequest = NSFetchRequest<Place>()
-      fetchRequest.entity = Place.entity()
-      fetchRequest.sortDescriptors = []
-
-      return NSFetchedResultsController<Place>(
-        fetchRequest: fetchRequest,
-        managedObjectContext: viewContext,
-        sectionNameKeyPath: nil,
-        cacheName: nil
-      )
-    }()
-
     super.init()
 
     fetchTimelineController.delegate = self
     fetchShowTweetController.delegate = self
-    fetchTweetController.delegate = self
-    fetchUserController.delegate = self
-    fetchPlaceController.delegate = self
-    fetchPollController.delegate = self
-    fetchMediaController.delegate = self
 
     try! fetchTimelineController.performFetch()
     try! fetchShowTweetController.performFetch()
-    try! fetchTweetController.performFetch()
-    try! fetchUserController.performFetch()
-    try! fetchPlaceController.performFetch()
-    try! fetchPollController.performFetch()
-    try! fetchMediaController.performFetch()
 
     updateTimeLine()
   }
@@ -174,7 +94,7 @@ final class ReverseChronologicalViewModel: NSObject, ReverseChronologicalTweetsV
 
     try response.places.forEach { place in
       try addPlace(place)
-    }
+    }    
   }
 
   func fetchTweets(last lastTweetID: String?, paginationToken: String?) async {
