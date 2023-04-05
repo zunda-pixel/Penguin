ba57cbe58ba10366b3b539846817a1faaf1a1425
zunda <zunda.dev@gmail.com>
2023-04-05 20:42:12 +0900

    replace to placeHolder

--- PenguinKit/Sources/View/Tweet/TweetCellView.swift
+++ PenguinKit/Sources/View/Tweet/TweetCellView.swift
@@ -174,19 +174,6 @@ struct TweetCellView<ViewModel: TweetCellViewProtocol>: View {
 
 struct TweetCellView_Previews: PreviewProvider {
   static var previews: some View {
-    let viewModel = TweetCellViewModel(
-      userID: "userID",
-      tweet: .init(id: "id", text: "text", createdAt: .now.addingTimeInterval(-1000)),
-      author: .init(
-        id: "id", name: "name", userName: "userName", verified: true,
-        profileImageURL: URL(
-          string: "https://pbs.twimg.com/profile_images/974322170309390336/tY8HZIhk_400x400.jpg")!),
-      retweet: nil,
-      quoted: nil,
-      medias: [],
-      polls: [],
-      places: []
-    )
-    TweetCellView(viewModel: viewModel)
+    TweetCellView(viewModel: TweetCellViewModel.placeHolder)
   }
 }
