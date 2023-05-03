1fd05394c19d4f951d6300cabb9d5ad806deb661
zunda <zunda.dev@gmail.com>
2023-05-04 01:52:43 +0900

    replace to tweetContent

--- PenguinKit/Sources/Other/Reply.swift
+++ PenguinKit/Sources/Other/Reply.swift
@@ -6,17 +6,17 @@ import Foundation
 import Sweet
 
 struct Reply: Identifiable {
-  var id: String { replyID }
-  let replyID: String
-  let ownerID: String
+  let id: UUID
+  let tweetContent: TweetContentModel
   let replyUsers: [Sweet.UserModel]
 
-  init(replyID: String, ownerID: String, replyUsers: [Sweet.UserModel]) {
-    self.replyID = replyID
-    self.ownerID = ownerID
+  init(tweetContent: TweetContentModel, replyUsers: [Sweet.UserModel]) {
+    self.id = UUID()
+    
+    self.tweetContent = tweetContent
     // ownerが先頭に来るようにする
     self.replyUsers = replyUsers.sorted { user1, user2 in
-      user1.id == ownerID && user2.id != ownerID
+      user1.id == tweetContent.author.id && user2.id != tweetContent.author.id
     }
   }
 }
