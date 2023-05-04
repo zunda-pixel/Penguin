8096d47d0f34d7919d0cfe4c8f088315a6832552
zunda <zunda.dev@gmail.com>
2023-05-04 22:40:42 +0900

    add placeHolderView

--- PenguinKit/Sources/View/User/OnlineUserDetailView.swift
+++ PenguinKit/Sources/View/User/OnlineUserDetailView.swift
@@ -3,6 +3,7 @@
 //
 
 import SwiftUI
+import Sweet
 
 struct OnlineUserDetailView: View {
   @StateObject var viewModel: OnlineUserDetailViewModel
@@ -21,14 +22,75 @@ struct OnlineUserDetailView: View {
     VStack {
       if let user = viewModel.targetUser {
         let viewModel: UserDetailViewModel = .init(userID: viewModel.userID, user: user)
-
+        
         UserDetailView(viewModel: viewModel)
       } else {
-        ProgressView()
+        placeHolderView
       }
     }
     .task {
       await fetchUser()
     }
+  }
+  
+  var placeHolderView: some View {
+    List {
+      VStack {
+        let user = Sweet.UserModel.placeHolder
+        ProfileImageView(url: user.profileImageURL!)
+          .frame(width: 100, height: 100)
+        UserProfileView(viewModel: .init(user: user))
+        
+        VStack {
+          let buttonWidth: CGFloat = 200
+          
+          Button { } label: {
+            Label("DirectMessage", systemImage: "envelope.fill")
+              .frame(maxWidth: buttonWidth * 2)
+          }
+
+          HStack {
+            Button { } label: {
+              Label("FOLLOWERS", systemImage: "figure.wave")
+                .frame(maxWidth: buttonWidth)
+            }
+            Button { } label: {
+              Label("FOLLOWING", systemImage: "figure.walk")
+                .frame(maxWidth: buttonWidth)
+            }
+          }
+          HStack {
+            Button { } label: {
+              Label("Like", systemImage: "heart")
+                .frame(maxWidth: buttonWidth)
+            }
+            Button { } label: {
+              Label("List", systemImage: "list.dash.header.rectangle")
+                .frame(maxWidth: buttonWidth)
+            }
+          }
+        }
+        
+        Divider()
+      }
+        .padding()
+        .listRowInsets(EdgeInsets())
+        .buttonStyle(.bordered)
+        .buttonBorderShape(.roundedRectangle)
+        .listRowSeparator(.hidden)
+      
+      ForEach(0..<100) { _ in
+        VStack {
+          TweetCellView(viewModel: TweetCellViewModel.placeHolder)
+            .padding(EdgeInsets(top: 3, leading: 10, bottom: 0, trailing: 10))
+          Divider()
+        }
+        .listRowInsets(EdgeInsets())
+      }
+      .listContentAttribute()
+      .listRowSeparator(.hidden)
+    }
+    .listStyle(.inset)
+    .redacted(reason: .placeholder)
   }
 }
