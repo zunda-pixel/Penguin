95fd47b29b33064642aafd476c609ef261907204
zunda <zunda.dev@gmail.com>
2023-05-07 00:43:06 +0900

    add AccountDetailViewModel

--- PenguinKit/Sources/View/User/AccountSettingView.swift
+++ PenguinKit/Sources/View/User/AccountSettingView.swift
@@ -6,29 +6,27 @@ import Sweet
 import SwiftUI
 
 struct AccountDetailView: View {
-  let userID: String
-  let user: Sweet.UserModel
+  let viewModel: AccountDetailViewModel
 
   var body: some View {
     List {
       Group {
         let mutingUsersViewModel = MutingUsersViewModel(
-          userID: userID,
-          ownerID: user.id
+          userID: viewModel.userID,
+          ownerID: viewModel.user.id
         )
         NavigationLink(value: mutingUsersViewModel) {
           Label("Mute", systemImage: "speaker.slash")
         }
 
         let blockingUsersViewModel = BlockingUsersViewModel(
-          userID: userID,
-          ownerID: user.id
+          userID: viewModel.userID,
+          ownerID: viewModel.user.id
         )
         NavigationLink(value: blockingUsersViewModel) {
           Label("Block", systemImage: "person.crop.circle.badge.xmark")
         }
       }
-      .listContentAttribute()
     }
     .scrollViewAttitude()
   }
@@ -37,10 +35,10 @@ struct AccountDetailView: View {
 struct AccountDetailView_Preview: PreviewProvider {
   static var previews: some View {
     NavigationStack {
-      AccountDetailView(
+      AccountDetailView(viewModel: .init(
         userID: "userID",
         user: .init(id: "id", name: "name", userName: "userName")
-      )
+      ))
     }
   }
 }
---
+++ PenguinKit/Sources/ViewModel/Users/AccountDetailViewModel.swift
@@ -0,0 +1,11 @@
+//
+//  AccountDetailViewModel.swift
+//
+
+import Foundation
+import Sweet
+
+struct AccountDetailViewModel: Hashable {
+  let userID: String
+  let user: Sweet.UserModel
+}
