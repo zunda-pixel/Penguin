c05e271d2293cfc0e64fbfb3d6bdc200deabc4a3
zunda <zunda.dev@gmail.com>
2023-05-08 11:41:04 +0900

    move to Extension

--- PenguinKit/Sources/Env.swift
+++ PenguinKit/Sources/Env.swift
@@ -7,9 +7,4 @@ import Foundation
 enum Env {
   static let clientKey: String = <#CLIENT_KEY#>
   static let clientSecretKey: String = <#CLIENT_SECRET_KEY#>
-  
-  static let teamID: String = Bundle.main.infoDictionary!["AppIdentifierPrefix"] as! String
-
-  static let appGroups: String = "group.zunda.penguin"
-  static let schemeURL: URL = .init(string: "penguin://")!
 }
