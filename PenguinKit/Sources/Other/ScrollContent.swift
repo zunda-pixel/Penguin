a25097f9d5dc321cadcb58415dd856ada11250e9
zunda <zunda.dev@gmail.com>
2023-05-09 12:59:47 +0900

    add ScrollContent

---
+++ PenguinKit/Sources/Other/ScrollContent.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollContent.swift
+//
+
+import SwiftUI
+
+struct ScrollContent<Content: Hashable>: Equatable {
+  let id: UUID
+  let contentID: Content
+  let anchor: UnitPoint
+  
+  init(contentID: Content, anchor: UnitPoint) {
+    self.id = UUID()
+    self.contentID = contentID
+    self.anchor = anchor
+  }
+}
