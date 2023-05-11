348cebb3ccd177c0472a3a670308353eb1012abb
zunda <zunda.dev@gmail.com>
2023-05-11 18:20:24 +0900

    add ScrollPoint

--- PenguinKit/Sources/Other/ScrollContent.swift
+++ PenguinKit/Sources/Other/ScrollContent.swift
@@ -7,11 +7,26 @@ import SwiftUI
 struct ScrollContent<Content: Hashable>: Equatable {
   let id: UUID
   let contentID: Content
-  let anchor: UnitPoint
+  let anchor: ScrollPoint
 
-  init(contentID: Content, anchor: UnitPoint) {
+  init(
+    contentID: Content,
+    anchor: ScrollPoint
+  ) {
     self.id = UUID()
     self.contentID = contentID
     self.anchor = anchor
+  }
+}
+
+enum ScrollPoint {
+  case top, bottom, center
+  
+  var unitPoint: UnitPoint {
+    switch self {
+    case .top: return .top
+    case .center: return .center
+    case .bottom: return .bottom
+    }
   }
 }
