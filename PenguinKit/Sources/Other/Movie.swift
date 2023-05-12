3d7840da33b2396999e831a72c05519b814454eb
zunda <zunda.dev@gmail.com>
2023-05-13 02:41:02 +0900

    update Movie

--- PenguinKit/Sources/Other/Movie.swift
+++ PenguinKit/Sources/Other/Movie.swift
@@ -11,9 +11,13 @@ struct Movie: Transferable {
     FileRepresentation(contentType: .movie) { movie in
       SentTransferredFile(movie.url)
     } importing: { receivedData in
+      let fileManager = FileManager.default
       let fileName = receivedData.file.lastPathComponent
-      let copy: URL = FileManager.default.temporaryDirectory.appendingPathComponent(fileName)
-      try FileManager.default.copyItem(at: receivedData.file, to: copy)
+      let copy: URL = fileManager.temporaryDirectory.appendingPathComponent(fileName)
+      if fileManager.fileExists(atPath: copy.path()) {
+        try fileManager.removeItem(at: copy)
+      }
+      try fileManager.copyItem(at: receivedData.file, to: copy)
       return .init(url: copy)
     }
   }
