3df034d57603fda7026d5608e481f08d83b4cdb6
zunda <zunda.dev@gmail.com>
2023-05-20 21:29:56 +0900

    change to group

--- PenguinKit/Sources/View/BaseView/OGPCardView.swift
+++ PenguinKit/Sources/View/BaseView/OGPCardView.swift
@@ -24,7 +24,7 @@ struct OGPCardView: View {
         .resizable()
         .scaledToFit()
 
-      VStack {
+      Group {
         Text(urlModel.displayURL!)
           .foregroundStyle(settings.colorType.colorSet.tintColor)
 
