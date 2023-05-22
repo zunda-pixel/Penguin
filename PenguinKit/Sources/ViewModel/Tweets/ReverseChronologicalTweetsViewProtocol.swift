43f96e9b022c24242cf631bad46893586b0abc06
zunda <zunda.dev@gmail.com>
2023-05-22 20:02:21 +0900

    move to ReverseChronologicalTweetsViewProtocol

--- PenguinKit/Sources/ViewModel/Tweets/ReverseChronologicalTweetsViewProtocol.swift
+++ PenguinKit/Sources/ViewModel/Tweets/ReverseChronologicalTweetsViewProtocol.swift
@@ -18,9 +18,58 @@ protocol ReverseChronologicalTweetsViewProtocol: ObservableObject {
   func containsTimelineDataBase(tweetID: String) throws -> Bool
   func setTimelines() async
   func fetchTweets(last lastTweetID: String?, paginationToken: String?) async
+  func getTweet(_ tweetID: String) -> Tweet?
+  func getUser(_ userID: String) -> User?
+  func getPlaces(_ placeIDs: [String]) -> [Place]
+  func getPolls(_ pollIDs: [String]) -> [Poll]
+  func getMedias(_ mediaIDs: [String]) -> [Media]
 }
 
 extension ReverseChronologicalTweetsViewProtocol {
+  func getTweet(_ tweetID: String) -> Tweet? {
+    let request = Tweet.fetchRequest()
+    request.predicate = .init(format: "id = %@", tweetID)
+    request.fetchLimit = 1
+
+    let tweets = try! backgroundContext.fetch(request)
+
+    return tweets.first
+  }
+
+  func getPlaces(_ placeIDs: [String]) -> [Place] {
+    let request = Place.fetchRequest()
+    request.predicate = .init(format: "id IN %@", placeIDs)
+    request.fetchLimit = placeIDs.count
+
+    return try! backgroundContext.fetch(request)
+  }
+
+  func getPolls(_ pollIDs: [String]) -> [Poll] {
+    let request = Poll.fetchRequest()
+    request.predicate = .init(format: "id IN %@", pollIDs)
+    request.fetchLimit = pollIDs.count
+
+    return try! backgroundContext.fetch(request)
+  }
+
+  func getMedias(_ mediaIDs: [String]) -> [Media] {
+    let request = Media.fetchRequest()
+    request.predicate = .init(format: "key IN %@", mediaIDs)
+    request.fetchLimit = mediaIDs.count
+
+    return try! backgroundContext.fetch(request)
+  }
+  
+  func getUser(_ userID: String) -> User? {
+    let request = User.fetchRequest()
+    request.predicate = .init(format: "id = %@", userID)
+    request.fetchLimit = 1
+
+    let users = try! backgroundContext.fetch(request)
+
+    return users.first
+  }
+  
   func addResponse(response: Sweet.TweetsResponse) throws {
     let tweets = response.tweets + response.relatedTweets
 
@@ -62,6 +111,89 @@ extension ReverseChronologicalTweetsViewProtocol {
         objects: response.places.map { $0.dictionaryValue() }
       )
       try backgroundContext.execute(request)
+    }
+    
+    try backgroundContext.save()
+        
+    let decoder = JSONDecoder.twitter
+    
+    for tweet in response.tweets {
+      let tweetCell = TweetCell(context: backgroundContext)
+      
+      let tweetContent = TweetContent(context: backgroundContext)
+      tweetContent.tweet = getTweet(tweet.id)!
+      tweetContent.author = getUser(tweet.authorID!)!
+      tweetCell.tweetContent = tweetContent
+      
+      let retweetID = tweet.referencedTweets.first { $0.type == .retweeted }?.id
+      let retweet = retweetID.map { getTweet($0)! }
+      let retweetAuthor = retweet.map { getUser($0.authorID!)! }
+      
+      if let retweet, let retweetAuthor {
+        let retweetContent = TweetContent(context: backgroundContext)
+        retweetContent.tweet = retweet
+        retweetContent.author = retweetAuthor
+        tweetCell.retweet = retweetContent
+      }
+      
+      let quotedID: String?
+      
+      if let quoted = tweet.referencedTweets.first(where: { $0.type == .quoted }) {
+        quotedID = quoted.id
+      } else {
+        let retweetReferenced = retweet?.referencedTweets.map {
+          try! decoder.decode([Sweet.ReferencedTweetModel].self, from: $0)
+        }
+        quotedID = retweetReferenced?.first { $0.type == .quoted }?.id
+      }
+      
+      if let quotedID {
+        let quoted = QuotedTweetContent(context: backgroundContext)
+        
+        let quotedTweetContent = TweetContent(context: backgroundContext)
+        quotedTweetContent.tweet = getTweet(quotedID)!
+        quotedTweetContent.author = getUser(quotedTweetContent.tweet!.authorID!)!
+        quoted.tweetContent = quotedTweetContent
+
+        let quotedReferenced = try! decoder.decode(
+          [Sweet.ReferencedTweetModel].self,
+          from: quotedTweetContent.tweet!.referencedTweets!
+        )
+        
+        if let quotedID = quotedReferenced.first(where: { $0.type == .quoted })?.id {
+          if let tweet = getTweet(quotedID) {
+            let quotedContent = TweetContent(context: backgroundContext)
+            quotedContent.tweet = tweet
+            quotedContent.author = getUser(tweet.authorID!)!
+            quoted.quoted = quotedContent
+          }
+        }
+        
+        tweetCell.quoted = quoted
+      }
+      
+      let tweets = [
+        tweetCell.tweetContent?.tweet,
+        tweetCell.retweet?.tweet,
+        tweetCell.quoted?.tweetContent?.tweet,
+        tweetCell.quoted?.quoted?.tweet
+      ].compacted()
+      
+      let attachments = tweets.compactMap(\.attachments).compactMap {
+        return try! decoder.decode(Sweet.AttachmentsModel.self, from: $0)
+      }
+      
+      let mediaKeys = attachments.flatMap { $0.mediaKeys }
+      tweetCell.medias = Set(getMedias(Array(mediaKeys.uniqued()))) as NSSet
+      let pollIDs = attachments.compactMap { $0.pollID }
+      tweetCell.polls = Set(getPolls(Array(pollIDs.uniqued()))) as NSSet
+      
+      let placeIDs = tweets.compactMap(\.geo).compactMap {
+        let geo = try! decoder.decode(Sweet.SimpleGeoModel.self, from: $0)
+        return geo.placeID
+      }
+      
+      tweetCell.places = Set(getPlaces(Array(placeIDs.uniqued()))) as NSSet
     }
   }
 
--- PenguinKit/Sources/ViewModel/Tweets/TweetCellViewProvider.swift
+++ PenguinKit/Sources/ViewModel/Tweets/TweetCellViewProvider.swift
@@ -13,6 +13,16 @@ struct TweetCellViewProvider {
     backgroundContext = PersistenceController.shared.container.newBackgroundContext()
     backgroundContext.mergePolicy = NSMergePolicy.mergeByPropertyObjectTrump
   }
+  
+  func getUsers(screenIDs: [String]) -> [Sweet.UserModel] {
+    let request = User.fetchRequest()
+    request.predicate = .init(format: "userName IN %@", screenIDs)
+    request.fetchLimit = screenIDs.count
+
+    let users = try! backgroundContext.fetch(request)
+
+    return users.map { .init(user: $0) }
+  }
 
   func reply(viewModel: TweetCellViewModel) async -> Reply {
     let mentions = viewModel.tweet.entity?.mentions ?? []
@@ -33,146 +43,38 @@ struct TweetCellViewProvider {
     )
   }
 
-  func getTweet(_ tweetID: String) -> Sweet.TweetModel? {
-    let request = Tweet.fetchRequest()
-    request.predicate = .init(format: "id = %@", tweetID)
-    request.fetchLimit = 1
-
-    let tweets = try! backgroundContext.fetch(request)
-
-    return tweets.first.map { .init(tweet: $0) }
-  }
-
-  func getPlaces(_ placeIDs: [String]) -> [Sweet.PlaceModel] {
-    let request = Place.fetchRequest()
-    request.predicate = .init(format: "id IN %@", placeIDs)
-    request.fetchLimit = placeIDs.count
-
-    let places = try! backgroundContext.fetch(request)
-
-    return places.map { .init(place: $0) }
-  }
-
-  func getPolls(_ pollIDs: [String]) -> [Sweet.PollModel] {
-    let request = Poll.fetchRequest()
-    request.predicate = .init(format: "id IN %@", pollIDs)
-    request.fetchLimit = pollIDs.count
-
-    let polls = try! backgroundContext.fetch(request)
-
-    return polls.map { .init(poll: $0) }
-  }
-
-  func getMedias(_ mediaIDs: [String]) -> [Sweet.MediaModel] {
-    let request = Media.fetchRequest()
-    request.predicate = .init(format: "key IN %@", mediaIDs)
-    request.fetchLimit = mediaIDs.count
-
-    let medias = try! backgroundContext.fetch(request)
-
-    return medias.map { .init(media: $0) }
-  }
-
-  func getUsers(screenIDs: [String]) -> [Sweet.UserModel] {
-    let request = User.fetchRequest()
-    request.predicate = .init(format: "userName IN %@", screenIDs)
-    request.fetchLimit = screenIDs.count
-
-    let users = try! backgroundContext.fetch(request)
-
-    return users.map { .init(user: $0) }
-  }
-
-  func getUser(_ userID: String) -> Sweet.UserModel? {
-    let request = User.fetchRequest()
-    request.predicate = .init(format: "id = %@", userID)
-    request.fetchLimit = 1
-
-    let users = try! backgroundContext.fetch(request)
-
-    return users.first.map { .init(user: $0) }
-  }
-
-  func retweetContent(tweet: Sweet.TweetModel) -> TweetContentModel? {
-    let retweet = tweet.referencedTweets.first(where: { $0.type == .retweeted })
-
-    guard let retweet else { return nil }
-
-    let tweet = getTweet(retweet.id)!
-    let user = getUser(tweet.authorID!)!
-
-    return TweetContentModel(tweet: tweet, author: user)
-  }
-
-  func quotedContent(tweet: Sweet.TweetModel, retweet: Sweet.TweetModel?) -> QuotedTweetModel? {
-    let quotedTweetID: String?
-
-    if let quoted = tweet.referencedTweets.first(where: { $0.type == .quoted }) {
-      quotedTweetID = quoted.id
-    } else if let quoted = retweet?.referencedTweets.first(where: { $0.type == .quoted }) {
-      quotedTweetID = quoted.id
-    } else {
-      quotedTweetID = nil
-    }
-
-    guard let quotedTweetID else { return nil }
-
-    let tweet = getTweet(quotedTweetID)!
-    let user = getUser(tweet.authorID!)!
-
-    let quotedQuotedTweet: TweetContentModel?
-
-    if let quoted = tweet.referencedTweets.first(where: { $0.type == .quoted }),
-      // TODO 引用先のツイートが非公開アカウントのツイートの可能性があるためif letアンラップ
-      let tweet = getTweet(quoted.id)
-    {
-      let user = getUser(tweet.authorID!)!
-      quotedQuotedTweet = TweetContentModel(tweet: tweet, author: user)
-    } else {
-      quotedQuotedTweet = nil
-    }
-
-    return QuotedTweetModel(
-      tweetContent: .init(tweet: tweet, author: user),
-      quoted: quotedQuotedTweet
-    )
-  }
-
   func getTweetCellViewModel(userID: String, tweetID: String) async -> TweetCellViewModel {
     return await backgroundContext.perform {
-      let tweet = self.getTweet(tweetID)!
-
-      let author = self.getUser(tweet.authorID!)!
-
-      let retweet: TweetContentModel? = self.retweetContent(tweet: tweet)
-
-      let quoted: QuotedTweetModel? = self.quotedContent(tweet: tweet, retweet: retweet?.tweet)
-
-      let tweets = [
-        tweet,
-        retweet?.tweet,
-        quoted?.tweetContent.tweet,
-        quoted?.quoted?.tweet,
-      ].compacted()
-
-      let mediaKeys = tweets.compactMap(\.attachments).flatMap(\.mediaKeys)
-      let medias = self.getMedias(Array(mediaKeys.uniqued()))
-
-      let pollIDs = tweets.compactMap(\.attachments).compactMap(\.pollID)
-      let polls = self.getPolls(pollIDs)
-
-      let placeIDs = tweets.compactMap(\.geo).compactMap(\.placeID)
-      let places = self.getPlaces(placeIDs)
+      let request = TweetCell.fetchRequest()
+      let tweetCell = try! backgroundContext.fetch(request).first { ($0 as TweetCell).tweetContent!.tweet!.id! == tweetID }!
 
       let viewModel: TweetCellViewModel = .init(
         userID: userID,
-        tweet: tweet,
-        author: author,
-        retweet: retweet,
-        quoted: quoted,
-        medias: medias,
-        polls: polls,
-        places: places
+        tweet: Sweet.TweetModel(tweet: tweetCell.tweetContent!.tweet!),
+        author: Sweet.UserModel(user: tweetCell.tweetContent!.author!),
+        retweet: tweetCell.retweet.map {
+          TweetContentModel(
+            tweet: Sweet.TweetModel(tweet: $0.tweet!),
+            author: Sweet.UserModel(user: $0.author!)
+          )
+        },
+        quoted: tweetCell.quoted.map {
+          QuotedTweetModel(
+            tweetContent: TweetContentModel(
+              tweet: Sweet.TweetModel(tweet: $0.tweetContent!.tweet!),
+              author: Sweet.UserModel(user: $0.tweetContent!.author!)
+            ),
+            quoted: $0.quoted.map {
+              TweetContentModel(
+                tweet: Sweet.TweetModel(tweet: $0.tweet!),
+                author: Sweet.UserModel(user: $0.author!)
+              )
+            }
+          )
+        },
+        medias: tweetCell.medias!.map { Sweet.MediaModel(media: $0 as! Media) },
+        polls: tweetCell.polls!.map { Sweet.PollModel(poll: $0 as! Poll) },
+        places: tweetCell.places!.map { Sweet.PlaceModel(place: $0 as! Place) }
       )
 
       return viewModel
